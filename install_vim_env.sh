35848455bb4290efbb2561d853f7c4b6dffab0cd
Oskar Plaska <oskar.plaska@gmail.com>
2021-10-27 17:33:08 +0200

    Improve backup of previous vim configuration

--- install_vim_env.sh
+++ install_vim_env.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-
+set -e
 #Designed for Vim 8.0 and above
 
-cd ~/
-if [[ -f "~/.vimrc" ]]; then
-    mv -f ~/.vimrc ~/.vimrc.backup
+cd $HOME
+if [ -e ".vimrc" ]; then
+    mv -f .vimrc .vimrc.backup
+    echo "Created backup of .vimrc"
 fi
-wget https://raw.githubusercontent.com/oskpla/tools-env/master/.vimrc
 
-mkdir -p ~/.vim/pack/plugins/start
-git clone --depth=1 https://github.com/ctrlpvim/ctrlp.vim.git ~/.vim/pack/plugins/start/ctrlp
+echo "Downloading .vimrc..."
+wget -q https://raw.githubusercontent.com/oskpla/tools-env/master/.vimrc
+
+if [ -d ".vim" ]; then
+    rm -fr .vim_backup
+    mv -f .vim .vim_backup
+    echo "Created backup of .vim"
+fi
+
+echo "Creating vim plugin directory..."
+mkdir -p .vim/pack/plugins/start
+
+echo "Cloning 'ctrlp' plugin..."
+git clone --depth=1 https://github.com/ctrlpvim/ctrlp.vim.git .vim/pack/plugins/start/ctrlp > /dev/null 2>&1
+echo "Done"
+
 
